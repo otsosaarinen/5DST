153fc905960166431f3ade51a409079cb7064043
Otso Saarinen <saarinenotso@gmail.com>
2025-05-13 13:29:54 +0300

    add images between the list items

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,13 @@ function App() {
         <h1 className="text-5xl font-bold italic underline decoration-[#4981FF] decoration-3 underline-offset-8">
           <span className="text-[#4981FF]">5</span> DIGIT SUOMI TURNAUS
         </h1>
-        <ul className="flex flex-row gap-5">
+        <ul className="flex flex-row items-center justify-center gap-3">
           <li className="hover:cursor-pointer hover:underline">Discord</li>
+          <img className="h-4 w-4" src="/favicon.png" alt="star" />
           <li className="hover:cursor-pointer hover:underline">Sheet</li>
+          <img className="h-4 w-4" src="/favicon.png" alt="star" />
           <li className="hover:cursor-pointer hover:underline">Twitch</li>
+          <img className="h-4 w-4" src="/favicon.png" alt="star" />
           <li className="hover:cursor-pointer hover:underline">Challonge</li>
         </ul>
       </div>
