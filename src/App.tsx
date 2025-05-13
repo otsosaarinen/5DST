2ee0af3abf446d785e50a957ef11751cc1fa6bd2
Otso Saarinen <saarinenotso@gmail.com>
2025-05-13 13:23:15 +0300

    add header and links

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,17 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div className="">5DST</div>
+      <div className="font-poppins flex h-screen w-screen flex-col items-center justify-center gap-5 bg-[#1E1E1E] text-white">
+        <h1 className="text-5xl font-bold italic underline decoration-[#4981FF] decoration-3 underline-offset-8">
+          <span className="text-[#4981FF]">5</span> DIGIT SUOMI TURNAUS
+        </h1>
+        <ul className="flex flex-row gap-5">
+          <li className="hover:cursor-pointer hover:underline">Discord</li>
+          <li className="hover:cursor-pointer hover:underline">Sheet</li>
+          <li className="hover:cursor-pointer hover:underline">Twitch</li>
+          <li className="hover:cursor-pointer hover:underline">Challonge</li>
+        </ul>
+      </div>
     </>
   );
 }
