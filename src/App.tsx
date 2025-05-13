c37dc7375cd54171b08b24f60064f21c78577446
Otso Saarinen <saarinenotso@gmail.com>
2025-05-13 13:38:42 +0300

    add twitch embed and check if embed is already rendered

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,29 @@
 import "./App.css";
+import { useEffect, useRef } from "react";
 
 function App() {
+  const embedInitialized = useRef(false);
+
+  useEffect(() => {
+    if (embedInitialized.current) return;
+    embedInitialized.current = true;
+
+    const script = document.createElement("script");
+    script.src = "https://embed.twitch.tv/embed/v1.js";
+    script.async = true;
+    script.onload = () => {
+      if ((window as any).Twitch) {
+        new (window as any).Twitch.Embed("twitch-embed", {
+          width: 854,
+          height: 480,
+          channel: "nepicktournaments",
+          parent: [window.location.hostname],
+        });
+      }
+    };
+    document.body.appendChild(script);
+  }, []);
+
   return (
     <>
       <div className="font-poppins flex h-screen w-screen flex-col items-center justify-center gap-5 bg-[#1E1E1E] text-white">
@@ -16,6 +39,7 @@ function App() {
           <img className="h-4 w-4" src="/favicon.png" alt="star" />
           <li className="hover:cursor-pointer hover:underline">Challonge</li>
         </ul>
+        <section id="twitch-embed"></section>
       </div>
     </>
   );
