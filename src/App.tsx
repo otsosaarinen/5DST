4a6991a62e5fd724488b73e8243fe7947ea37026
Otso Saarinen <saarinenotso@gmail.com>
2025-05-13 13:40:01 +0300

    center text

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,7 @@ function App() {
   return (
     <>
       <div className="font-poppins flex h-screen w-screen flex-col items-center justify-center gap-5 bg-[#1E1E1E] text-white">
-        <h1 className="text-5xl font-bold italic underline decoration-[#4981FF] decoration-3 underline-offset-8">
+        <h1 className="text-center text-5xl font-bold italic underline decoration-[#4981FF] decoration-3 underline-offset-8">
           <span className="text-[#4981FF]">5</span> DIGIT SUOMI TURNAUS
         </h1>
         <ul className="flex flex-row items-center justify-center gap-3">
