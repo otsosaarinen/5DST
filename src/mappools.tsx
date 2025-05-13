6d265ed261df81460253cd5176bf15aecd339611
Otso Saarinen <saarinenotso@gmail.com>
2025-05-13 14:21:56 +0300

    create basic mappool page

--- src/mappools.tsx
+++ src/mappools.tsx
@@ -1,5 +1,14 @@
+import Header from "./Header";
+import "./App.css";
+
 function Mappools() {
-  return <></>;
+  return (
+    <>
+      <div className="font-poppins flex h-screen w-screen flex-col items-center justify-center bg-[#1E1E1E] text-white">
+        <Header />
+      </div>
+    </>
+  );
 }
 
 export default Mappools;
