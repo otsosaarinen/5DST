c2269442facc0315b0da168f5dbec62c170538e4
Otso Saarinen <saarinenotso@gmail.com>
2025-05-13 14:59:56 +0300

    add basic mappool css

--- src/mappools.tsx
+++ src/mappools.tsx
@@ -13,8 +13,23 @@ function Mappools() {
 
   return (
     <>
-      <div className="font-poppins flex h-screen w-screen flex-col items-center justify-center bg-[#1E1E1E] text-white">
+      <div className="font-poppins flex h-screen w-screen flex-col items-center justify-center gap-5 bg-[#1E1E1E] text-white">
         <Header />
+        <section className="w-250 bg-[#4981FF] p-3 text-3xl">
+          QUALIFIERS
+        </section>
+        <section className="w-250 bg-[#4981FF] p-3 text-3xl">RO32</section>
+        <section className="w-250 bg-[#4981FF] p-3 text-3xl">RO16</section>
+        <section className="w-250 bg-[#4981FF] p-3 text-3xl">
+          QUARTERFINALS
+        </section>
+        <section className="w-250 bg-[#4981FF] p-3 text-3xl">
+          SEMIFINALS
+        </section>
+        <section className="w-250 bg-[#4981FF] p-3 text-3xl">FINALS</section>
+        <section className="w-250 bg-[#4981FF] p-3 text-3xl">
+          GRAND FINALS
+        </section>
       </div>
     </>
   );
