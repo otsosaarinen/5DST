2cd61d89ab7f20329095720c827c785fcef8f055
Otso Saarinen <saarinenotso@gmail.com>
2025-05-13 14:08:44 +0300

    configure routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router";
+import { BrowserRouter, Routes, Route } from "react-router";
 import App from "./App.tsx";
+import Mappools from "./mappools.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/mappoolit" element={<Mappools />} />
+      </Routes>
     </BrowserRouter>
   </StrictMode>,
 );
