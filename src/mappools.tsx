11580aa4172a096b8fbcf714547fcfe11abd2529
Otso Saarinen <saarinenotso@gmail.com>
2025-05-13 14:41:48 +0300

    create visibility variables for mappools

--- src/mappools.tsx
+++ src/mappools.tsx
@@ -1,7 +1,16 @@
 import Header from "./Header";
 import "./App.css";
+import { useState } from "react";
 
 function Mappools() {
+  const [qualifier, setQualifier] = useState(false);
+  const [ro32, setRo32] = useState(false);
+  const [ro16, setRo16] = useState(false);
+  const [quarterfinals, setQquarterfinals] = useState(false);
+  const [semifinals, setSemifinals] = useState(false);
+  const [finals, setfinals] = useState(false);
+  const [grandfinals, setgrandfinals] = useState(false);
+
   return (
     <>
       <div className="font-poppins flex h-screen w-screen flex-col items-center justify-center bg-[#1E1E1E] text-white">
