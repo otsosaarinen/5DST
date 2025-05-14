5a12fa251f2395671fb1d3f6816ca9af5bc7fdb8
Otso Saarinen <saarinenotso@gmail.com>
2025-05-14 13:41:35 +0300

    rename Mappools tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router";
 import App from "./App.tsx";
-import Mappools from "./mappools.tsx";
+import Mappools from "./Mappools.tsx";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/mappoolit" element={<Mappools />} />
-      </Routes>
-    </BrowserRouter>
-  </StrictMode>,
+    <StrictMode>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<App />} />
+                <Route path="/mappoolit" element={<Mappools />} />
+            </Routes>
+        </BrowserRouter>
+    </StrictMode>,
 );
