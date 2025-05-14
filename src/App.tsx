27462409ca20c67c2ca285ac75e0e772d8139b9e
Otso Saarinen <saarinenotso@gmail.com>
2025-05-14 13:51:24 +0300

    add info text and link to rules

--- src/App.tsx
+++ src/App.tsx
@@ -44,14 +44,26 @@ function App() {
 
     return (
         <>
-            <div className="font-poppins flex h-screen w-screen flex-col items-center justify-center gap-15 bg-[#1E1E1E] text-white">
+            <div className="font-poppins flex min-h-screen max-w-screen flex-col items-center justify-center gap-10 bg-[#1E1E1E] py-15 text-white">
                 <Header />
+                <section className="w-180 text-center text-xl">
+                    5DST3 (5 Digit Suomi Turnaus) on suomalainen osu!standard
+                    turnaus. Turnaukseen voivat osallistua kaikki suomalaiset
+                    pelaajat rank välillä #10,000 - #99,999. Turnauksen
+                    järjestävät Pickxarro ja Nev-. Tutustu turnaukseen alla
+                    olevien linkkien kautta.
+                </section>
                 <div className="flex flex-row gap-5">
                     <div className="bg-[#4981FF] px-4 py-2 text-center text-2xl font-medium transition-all duration-200 ease-in hover:bg-white hover:text-[#1E1E1E]">
                         <Link to="/mappoolit">Mappoolit</Link>
                     </div>
                     <div className="bg-[#4981FF] px-4 py-2 text-center text-2xl font-medium transition-all duration-200 ease-in hover:bg-white hover:text-[#1E1E1E]">
                         <Link to="/aikataulu">Aikataulu</Link>
+                    </div>
+                    <div className="bg-[#4981FF] px-4 py-2 text-center text-2xl font-medium transition-all duration-200 ease-in hover:bg-white hover:text-[#1E1E1E]">
+                        <Link to="https://docs.google.com/document/d/1dMhobggW53QaZDkMrQp4XrmGJzt8jXPt7OQfomySMW8/edit?tab=t.0">
+                            Säännöt
+                        </Link>
                     </div>
                 </div>
                 <section className="" id="twitch-embed"></section>
