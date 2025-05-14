286a6956f137115a5dfa5f59eb025a693861f4a4
Otso Saarinen <saarinenotso@gmail.com>
2025-05-14 13:59:23 +0300

    update routes

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router";
 import App from "./App.tsx";
-import Mappools from "./Mappools.tsx";
+import Schedule from "./Schedule.tsx";
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<App />} />
-                <Route path="/mappoolit" element={<Mappools />} />
+                <Route path="/aikataulu" element={<Schedule />} />
             </Routes>
         </BrowserRouter>
     </StrictMode>,
