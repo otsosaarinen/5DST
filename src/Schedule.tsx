00189375d21fd291f23bc5836b3a44670d381569
Otso Saarinen <saarinenotso@gmail.com>
2025-05-14 13:59:10 +0300

    add bg color

--- src/Schedule.tsx
+++ src/Schedule.tsx
@@ -1,5 +1,9 @@
 function Schedule() {
-    return <></>;
+    return (
+        <>
+            <div className="min-h-screen max-w-screen bg-[#1E1E1E]"></div>
+        </>
+    );
 }
 
 export default Schedule;
